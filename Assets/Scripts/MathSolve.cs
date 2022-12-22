908868f222f44c3ff0b2f561bdbe8453137e21b0
ieongch0106 <jieong.dev@gmail.com>
2022-12-22 13:13:11 +0800

    updated

---
+++ Assets/Scripts/MathSolve.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MathSolve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int result;
+    public TMP_InputField input;
+    public TMP_Text showMath;
+
+    void Start()
+    {
+        CreateMath();
+    }
+
+    public void CreateMath()
+    {
+        int a = Random.Range(1, 100);
+        int b = Random.Range(1, 100);
+
+        result = a + b;
+        showMath.text = a + " + " + b;
+    }
+
+    public void CheckAnswer(string ans)
+    {
+        if (int.Parse(ans) == result)
+        {
+            Debug.Log("correct");
+        }
+        else
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum SIDE { left, mid, right };
 public class Player : MonoBehaviour
 {
     public SIDE m_side = SIDE.mid;
-    private float max_health = 5;
+    private float max_health = 1;
     private float currentHealth;
-    private int player_health;
     float NewPos;
     float midPos;
     float finalPosX;
     float finalPosY;
     bool isJump;
+    bool isHit;
     bool isDead;
     public float speedDodge;
     private Animator animation_controller;
@@ -29,13 +30,13 @@ public class Player : MonoBehaviour
     {
         NewPos = -1.5f;
         midPos = -1.5f;
-        transform.position = new Vector3(transform.position.x, transform.position.y, 1f);
         animation_controller = GetComponent<Animator>();
         character_controller = GetComponent<CharacterController>();
-        speedDodge = 2f;
+        speedDodge = 3f;
         velocity = 6f;
-        player_health = 5;
         isDead = false;
+        isJump = false;
+        isHit = false;
         currentHealth = max_health;
         _healthBar.UpdateHealthBar(max_health, currentHealth);
     }
@@ -52,21 +53,15 @@ public class Player : MonoBehaviour
             footsteps.enabled = true;
             sliding.enabled = false;
             jumping.enabled = false;
-            } else {
-                footsteps.enabled = false;
-                sliding.enabled = false;
-                jumping.enabled = true;
             }
         }
 
-        if (player_health <= 0 && !isDead) {
+        if (currentHealth <= 0 && !isDead) {
             isDead = true;
-            velocity = 0;
-            animation_controller.SetTrigger("death");
+            animation_controller.Play("Death");
         }
 
-        if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("GettingHit")) {
-            velocity = Mathf.Lerp(-1f, 0, Time.deltaTime);
+        if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("GotHit")) {
             footsteps.enabled = false;
             sliding.enabled = false;
             jumping.enabled = false;
@@ -77,6 +72,24 @@ public class Player : MonoBehaviour
             sliding.enabled = false;
             jumping.enabled = false;
             gettingHit.enabled = false;
+            isHit = false;
+        } else if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("JumpOver")) {
+            velocity = 5.5f;
+            finalPosY = 1.5f;
+            if (animation_controller.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.80) {
+                isJump = false;
+            }
+        } else if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("Death")) {
+            character_controller.enabled = false;
+            footsteps.enabled = false;
+            sliding.enabled = false;
+            jumping.enabled = false;
+            gettingHit.enabled = false;
+            if (animation_controller.GetCurrentAnimatorStateInfo(0).normalizedTime > 1) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            }
+        } else if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("Idle")) {
+            velocity = 0;
         }
 
         if (Input.GetKeyDown(KeyCode.A) && m_side != SIDE.left) {
@@ -105,41 +118,50 @@ public class Player : MonoBehaviour
                 m_side = SIDE.mid;
             }
         }
+        
+        if (!character_controller.isGrounded) {
+            finalPosY -= gravity * Time.deltaTime; 
+        }
 
         finalPosX = Mathf.Lerp(finalPosX, NewPos, Time.deltaTime * speedDodge);
         float finalPosZ = velocity * Time.deltaTime;
         Vector3 movement = new Vector3(finalPosX - transform.position.x, finalPosY * Time.deltaTime, finalPosZ);
         character_controller.Move(movement);
-        Jump();
-    }
-
-    public void Jump() {
-        if (character_controller.isGrounded) {
-            isJump = false;
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                animation_controller.SetTrigger("isJumping");
-            }
-
-            if (animation_controller.GetCurrentAnimatorStateInfo(0).IsName("JumpOver")) {
-                isJump = true;
-                finalPosY = 6f;
-            }
-        } else {
-            finalPosY -= gravity * Time.deltaTime;
-        }
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Debug.Log(hit.transform.name);
-        if (hit.transform.name == "Obstacle" || hit.transform.name == "Cart" 
-        || hit.transform.name == "SM_Bld_Wall_01" || hit.transform.name == "SM_Env_Rock_012" 
-        || hit.transform.name == "SM_Bld_Stall_04" || hit.transform.name == "SM_Env_Rock_011" 
-        || hit.transform.name == "SM_Env_Hedge_01" || hit.transform.name == "SM_Bld_Wall_01 (1)") {
-            currentHealth = currentHealth-1;
-            _healthBar.UpdateHealthBar(max_health, currentHealth);
-            animation_controller.Play("GettingHit");
+    {   
+        if (hit.transform.name != "Road-Left" && hit.transform.name != "Road-Mid" && hit.transform.name != "Road-Right" && hit.transform.name != "Land" && hit.transform.name != "SM_Env_TreeLog_01" && hit.transform.name != "Land Left" && hit.transform.name != "Land Right" && !isHit) {
+            Debug.Log(hit.transform.name);
+            hit.transform.GetComponent<Collider>().isTrigger = true;
+            isHit = true;
+            currentHealth -= 1;
+            animation_controller.Play("GotHit");
+            Destroy(hit.gameObject);
         }
+
+        else if ((hit.transform.name == "Land" || hit.transform.name == "Land Left" || hit.transform.name == "Land Right" || hit.transform.name == "Water") && !isJump) {
+            isJump = true;
+            footsteps.enabled = false;
+            sliding.enabled = false;
+            gettingHit.enabled = false;
+            // jumping.enabled = true;
+            character_controller.enabled = false;
+            animation_controller.Play("Idle");
+        }
+            
+            // animation_controller.SetTrigger("isJumping");
+    // {
+    //     Debug.Log(hit.transform.name);
+    //     if (hit.transform.name == "Obstacle" || hit.transform.name == "Cart" 
+    //     || hit.transform.name == "SM_Bld_Wall_01" || hit.transform.name == "SM_Env_Rock_012" 
+    //     || hit.transform.name == "SM_Bld_Stall_04" || hit.transform.name == "SM_Env_Rock_011" 
+    //     || hit.transform.name == "SM_Env_Hedge_01" || hit.transform.name == "SM_Bld_Wall_01 (1)") {
+    //         currentHealth = currentHealth-1;
+    //         _healthBar.UpdateHealthBar(max_health, currentHealth);
+    //         animation_controller.Play("GettingHit");
+    //     }
+    // }
     }
 
     
