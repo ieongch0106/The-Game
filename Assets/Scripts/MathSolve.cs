86ab5489efe93bd50d6374cb06da037ce79438bd
Hung Do <70021320+hungmhdo@users.noreply.github.com>
2022-12-21 22:36:48 -0500

    Add Math

---
+++ Assets/Scripts/MathSolve.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MathSolve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int result;
+    public TMP_Text showMath;
+
+    void CreateMath()
+    {
+        int a = Random.Range(1, 100);
+        int b = Random.Range(1, 100);
+
+        result = a + b;
+        showMath.text = a + " + " + b;
+    }
+
+    public void CheckAnswer(string ans)
+    {
+        if (int.Parse(ans) == result)
+        {
+            Debug.Log("correct");
+        }
+    }
+}
